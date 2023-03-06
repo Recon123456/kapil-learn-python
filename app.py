d7705e694004e11332d1b206121cd58717c665de
kapil sharma <99503456+Recon123456@users.noreply.github.com>
2023-03-06 11:09:10 +0000

    added dynamic data and api routes

--- app.py
+++ app.py
@@ -1,8 +1,40 @@
-from flask import Flask,render_template
-app= Flask(__name__)
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+Courses = [
+  {
+    'id': 1,
+    'title': 'Angular',
+    'location': 'Online',
+    'country': 'India',
+    'Cost': 320
+  }, 
+  {
+    'id': 2,
+    'title': 'SQL',
+    'location': 'Online',
+    'country': 'India',
+    'Cost': 220
+  }, 
+  {
+    'id': 3,
+    'title': 'JAVA',
+    'location': 'Offline',
+    'country': 'India',
+    'Cost': 250
+  }
+]
+
+
 @app.route("/")
 def hello():
-  return render_template('home.html')
+  return render_template('home.html',jobs=Courses,Compnay="Kapil's")
 
-if __name__=='__main__':
-  app.run(host='0.0.0.0',debug=True)
+@app.route("/api/course")
+def list_course():
+  return jsonify(Courses)
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
